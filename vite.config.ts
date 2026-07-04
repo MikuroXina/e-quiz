3f65e1d25b0abf405a4c01a4c9628c856125436d
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-07-04 12:40:51 +0900

    Add oxcfmt config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  fmt: {
+    sortTailwindcss: true,
+  },
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
