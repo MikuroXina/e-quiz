3b8a5facc1db02a166cbfee339cb09c78b028dac
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-07-04 15:55:56 +0900

    Append courses list

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,33 @@
+import { Card, Surface, Typography } from "@heroui/react";
 import { NavBar } from "~/organisms/nav-bar";
 
+interface Course {
+  id: string;
+  name: string;
+}
+
 export default function Home() {
+  const courses: Course[] = [{ id: "foo1", name: "整数論I" }];
   return (
-    <div>
+    <>
       <title>ホーム - e-Quiz</title>
-      <NavBar title="ホーム" />
-    </div>
+      <div className="h-screen overflow-auto">
+        <Surface className="sticky top-0 z-10 drop-shadow-md">
+          <NavBar title="ホーム" />
+        </Surface>
+        <div className="h-full p-4">
+          <Typography type="h2">講座一覧</Typography>
+          <div className="flex flex-col gap-2">
+            {courses.map(({ id, name }) => (
+              <Card key={id}>
+                <Card.Content>
+                  <Typography type="h3">{name}</Typography>
+                </Card.Content>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
