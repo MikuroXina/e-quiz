82dbde09e555d2c3bd532aa04d4fca6789eb4e2e
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-07-05 11:00:41 +0900

    Fix vite plugin order

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,20 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite-plus";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   fmt: {
     sortTailwindcss: true,
   },
-  plugins: [tailwindcss(), reactRouter(), cloudflare()],
+  plugins: [
+    cloudflare({
+      viteEnvironment: { name: "ssr" },
+    }),
+    react(),
+    tailwindcss(),
+    reactRouter(),
+  ],
   server: {
     port: 8787,
   },
