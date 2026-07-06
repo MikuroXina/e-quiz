8cb3559840163811809e398fe05c38ad94316923
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-07-06 12:47:13 +0900

    Reveal entry

---
+++ app/entry.server.tsx
@@ -0,0 +1,42 @@
+import type { EntryContext } from "react-router";
+import { ServerRouter } from "react-router";
+import { isbot } from "isbot";
+import { renderToReadableStream } from "react-dom/server";
+
+export const streamTimeout = 5_000;
+
+export default async function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  routerContext: EntryContext,
+): Promise<Response> {
+  // https://httpwg.org/specs/rfc9110.html#HEAD
+  if (request.method.toUpperCase() === "HEAD") {
+    return new Response(null, {
+      status: responseStatusCode,
+      headers: responseHeaders,
+    });
+  }
+
+  const body = await renderToReadableStream(
+    <ServerRouter context={routerContext} url={request.url} />,
+    {
+      signal: request.signal,
+      onError(error: unknown) {
+        responseStatusCode = 500;
+        console.error(error);
+      },
+    },
+  );
+
+  if (isbot(request.headers.get("User-Agent"))) {
+    await body.allReady;
+  }
+
+  responseHeaders.set("Content-Type", "text/html");
+  return new Response(body, {
+    status: responseStatusCode,
+    headers: responseHeaders,
+  });
+}
